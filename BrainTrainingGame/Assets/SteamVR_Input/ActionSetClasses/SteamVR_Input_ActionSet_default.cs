ac943b36158bbe7dd1b6b5f579c2ad507cf1543e
Nonthaporn Nakphu <43634056+farm-nakphu@users.noreply.github.com>
2020-11-15 17:44:37 +0900

    Edit project setting

--- BrainTrainingGame/Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ BrainTrainingGame/Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -105,6 +105,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Vector2 TouchpadTouch
+        {
+            get
+            {
+                return SteamVR_Actions.default_TouchpadTouch;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get

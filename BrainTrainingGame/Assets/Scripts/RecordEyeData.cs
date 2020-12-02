33598454c5b19d67f4d55805b51c82d94b19fd56
Nonthaporn Nakphu <43634056+farm-nakphu@users.noreply.github.com>
2020-12-02 19:43:32 +0900

    Add sranipal framework (eye tracking) (in progress)

---
+++ BrainTrainingGame/Assets/Scripts/RecordEyeData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ViveSR.anipal.Eye;
+
+public class RecordEyeData : MonoBehaviour
+{
+    public void EyeCalibration()
+    {
+        SRanipal_Eye.LaunchEyeCalibration();
+    }
+
+
+
+}

93e6cd59512cbfb6cc357de6e2c605c5766f2ca0
Nonthaporn Nakphu <nonthaporn.nak@gmail.com>
2021-06-11 15:12:18 +0900

    Change eye tracking data rate to 120Hz (in progress)

---
+++ BrainTrainingGame/Assets/Scripts/Game/EyeDataRecord.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ViveSR.anipal.Eye;
+using System.Runtime.InteropServices;
+
+public class EyeDataRecord : MonoBehaviour
+{
+    private static DataManager dataManager;
+    private static CommunicationController communicationController;
+    private static Camera mainCamera;
+
+    private static EyeData_v2 eyeData = new EyeData_v2();
+    private static bool eye_callback_registered = false;
+
+    private static string FileName;
+    private static string DataDir;
+    private static bool isRecord = false;
+
+    private void Start()
+    {
+        dataManager = GetComponent<DataManager>();
+        communicationController = GetComponent<CommunicationController>();
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponentInChildren<Camera>();
+    }
+
+    private void Update()
+    {
+        if (SRanipal_Eye_Framework.Status != SRanipal_Eye_Framework.FrameworkStatus.WORKING) return;
+
+        if (SRanipal_Eye_Framework.Instance.EnableEyeDataCallback == true && eye_callback_registered == false)
+        {
+            SRanipal_Eye_v2.WrapperRegisterEyeDataCallback(Marshal.GetFunctionPointerForDelegate((SRanipal_Eye_v2.CallbackBasic)EyeCallback));
+            eye_callback_registered = true;
+        }
+        else if (SRanipal_Eye_Framework.Instance.EnableEyeDataCallback == false && eye_callback_registered == true)
+        {
+            SRanipal_Eye_v2.WrapperUnRegisterEyeDataCallback(Marshal.GetFunctionPointerForDelegate((SRanipal_Eye_v2.CallbackBasic)EyeCallback));
+            eye_callback_registered = false;
+        }
+    }
+
+    private void OnDisable()
+    {
+        Release();
+    }
+
+    void OnApplicationQuit()
+    {
+        Release();
+    }
+
+    /// <summary>
+    /// Release callback thread when disabled or quit
+    /// </summary>
+    private static void Release()
+    {
+        if (eye_callback_registered == true)
+        {
+            SRanipal_Eye_v2.WrapperUnRegisterEyeDataCallback(Marshal.GetFunctionPointerForDelegate((SRanipal_Eye_v2.CallbackBasic)EyeCallback));
+            eye_callback_registered = false;
+        }
+    }
+
+    /// <summary>
+    /// Required class for IL2CPP scripting backend support
+    /// </summary>
+    internal class MonoPInvokeCallbackAttribute : System.Attribute
+    {
+        public MonoPInvokeCallbackAttribute() { }
+    }
+
+    /// <summary>
+    /// Eye tracking data callback thread.
+    /// Reports data at ~120hz
+    /// MonoPInvokeCallback attribute required for IL2CPP scripting backend
+    /// </summary>
+    /// <param name="eye_data">Reference to latest eye_data</param>
+    [MonoPInvokeCallback]
+    private static void EyeCallback(ref EyeData_v2 eye_data)
+    {
+        Dictionary<string, string> eyeTrackingData = new Dictionary<string, string>();
+        VerboseData verboseData;
+        if(isRecord)
+        {
+            eyeData = eye_data;
+            SRanipal_Eye_API.GetEyeData_v2(ref eyeData);
+            SRanipal_Eye_v2.GetVerboseData(out verboseData);
+            eyeTrackingData["GtecTime"] = communicationController.ReceivedData.ToString();
+            eyeTrackingData["UnityTime"] = (Time.time * 1000).ToString();  //System.DateTime.Now.ToString("HH-mm-ss.fff");
+            
+            eyeTrackingData["CombinedEyeValidDataBitMask"] = verboseData.combined.eye_data.eye_data_validata_bit_mask.ToString();
+            eyeTrackingData["LeftEyeValidDataBitMask"] = verboseData.left.eye_data_validata_bit_mask.ToString();
+            eyeTrackingData["RightEyeValidDataBitMask"] = verboseData.right.eye_data_validata_bit_mask.ToString();
+            eyeTrackingData["CombinedEyeOpenness"] = verboseData.combined.eye_data.eye_openness.ToString();
+            eyeTrackingData["LeftEyeOpenness"] = verboseData.left.eye_openness.ToString();
+            eyeTrackingData["RightEyeOpenness"] = verboseData.right.eye_openness.ToString();
+            eyeTrackingData["CombinedPupilDiameter"] = verboseData.combined.eye_data.pupil_diameter_mm.ToString();
+            eyeTrackingData["LeftPupilDiameter"] = verboseData.left.pupil_diameter_mm.ToString();
+            eyeTrackingData["RightPupilDiameter"] = verboseData.right.pupil_diameter_mm.ToString();
+            eyeTrackingData["CombinedPupilPosition"] = verboseData.combined.eye_data.pupil_position_in_sensor_area.ToString();
+            eyeTrackingData["LeftPupilPosition"] = verboseData.left.pupil_position_in_sensor_area.ToString();
+            eyeTrackingData["RightPupilPosition"] = verboseData.right.pupil_position_in_sensor_area.ToString();
+            eyeTrackingData["CombinedGazeOrigin"] = verboseData.combined.eye_data.gaze_origin_mm.ToString();
+            eyeTrackingData["LeftGazeOrigin"] = verboseData.left.gaze_origin_mm.ToString();
+            eyeTrackingData["RightGazeOrigin"] = verboseData.right.gaze_origin_mm.ToString();
+            eyeTrackingData["CombinedGazeDirection"] = verboseData.combined.eye_data.gaze_direction_normalized.ToString();
+            eyeTrackingData["LeftGazeDirection"] = verboseData.left.gaze_direction_normalized.ToString();
+            eyeTrackingData["RightGazeDirection"] = verboseData.right.gaze_direction_normalized.ToString();
+
+            eyeTrackingData["LeftEyeWide"] = eyeData.expression_data.left.eye_wide.ToString();
+            eyeTrackingData["RightEyeWide"] = eyeData.expression_data.right.eye_wide.ToString();
+            eyeTrackingData["LeftEyeSqeeze"] = eyeData.expression_data.left.eye_squeeze.ToString();
+            eyeTrackingData["RightEyeSqeeze"] = eyeData.expression_data.right.eye_squeeze.ToString();
+            eyeTrackingData["LeftEyeFrown"] = eyeData.expression_data.left.eye_frown.ToString();
+            eyeTrackingData["RightEyeFrown"] = eyeData.expression_data.right.eye_frown.ToString();
+
+            Ray ray;
+            FocusInfo focusInfo;
+            bool focusStatus = SRanipal_Eye_v2.Focus(GazeIndex.COMBINE, out ray, out focusInfo);
+            //Debug.Log("FocusStatus: " + focusStatus);
+            eyeTrackingData["FocusStatus"] = focusStatus.ToString();
+            if (focusStatus)
+            {
+                Debug.Log(focusInfo.collider);
+                eyeTrackingData["RayOriginal"] = ray.origin.ToString();
+                eyeTrackingData["RayDirection"] = ray.direction.ToString();
+                eyeTrackingData["Collider"] = focusInfo.collider.ToString();
+                eyeTrackingData["Distance"] = focusInfo.distance.ToString();
+                eyeTrackingData["Normal"] = focusInfo.normal.ToString();
+                eyeTrackingData["Point"] = focusInfo.point.ToString();
+                eyeTrackingData["Transform.position"] = focusInfo.transform.position.ToString();
+                eyeTrackingData["Transform.rotation"] = focusInfo.transform.rotation.ToString();
+            }
+            else
+            {
+                eyeTrackingData["RayOriginal"] = "0";
+                eyeTrackingData["RayDirection"] = "0";
+                eyeTrackingData["Collider"] = "0";
+                eyeTrackingData["Distance"] = "0";
+                eyeTrackingData["Normal"] = "0";
+                eyeTrackingData["Point"] = "0";
+                eyeTrackingData["Transform.position"] = "0";
+                eyeTrackingData["Transform.rotation"] = "0";
+            }
+
+            eyeTrackingData["Camera.position"] = mainCamera.transform.position.ToString();
+            eyeTrackingData["Camera.rotation"] = mainCamera.transform.rotation.ToString();
+
+            dataManager.WriteData(DataDir, FileName, eyeTrackingData, false, false);
+        }
+
+    }
+
+    public void StartRecord(string fliename, string datadir)
+    {
+        FileName = fliename;
+        DataDir = datadir;
+        isRecord = true;
+    }
+
+    public void StopRecord()
+    {
+        isRecord = false;
+    }
+
+}
---
+++ BrainTrainingGame/Assets/Scripts/test120hz.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using ViveSR.anipal.Eye;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+/// <summary>
+/// Example usage for eye tracking callback
+/// Note: Callback runs on a separate thread to report at ~120hz.
+/// Unity is not threadsafe and cannot call any UnityEngine api from within callback thread.
+/// </summary>
+public class test120hz : MonoBehaviour
+{
+    private static EyeData eyeData = new EyeData();
+    private static bool eye_callback_registered = false;
+
+    public Text uiText;
+    private float updateSpeed = 0;
+    private static float lastTime, currentTime;
+
+
+    void Update()
+    {
+        if (SRanipal_Eye_Framework.Status != SRanipal_Eye_Framework.FrameworkStatus.WORKING) return;
+
+
+        if (SRanipal_Eye_Framework.Instance.EnableEyeDataCallback == true && eye_callback_registered == false)
+        {
+            SRanipal_Eye.WrapperRegisterEyeDataCallback(Marshal.GetFunctionPointerForDelegate((SRanipal_Eye.CallbackBasic)EyeCallback));
+            eye_callback_registered = true;
+        }
+        else if (SRanipal_Eye_Framework.Instance.EnableEyeDataCallback == false && eye_callback_registered == true)
+        {
+            SRanipal_Eye.WrapperUnRegisterEyeDataCallback(Marshal.GetFunctionPointerForDelegate((SRanipal_Eye.CallbackBasic)EyeCallback));
+            eye_callback_registered = false;
+        }
+
+        updateSpeed = currentTime - lastTime;
+        uiText.text = updateSpeed.ToString() + " ms";
+    }
+
+    private void OnDisable()
+    {
+        Release();
+    }
+
+    void OnApplicationQuit()
+    {
+        Release();
+    }
+
+    /// <summary>
+    /// Release callback thread when disabled or quit
+    /// </summary>
+    private static void Release()
+    {
+        if (eye_callback_registered == true)
+        {
+            SRanipal_Eye.WrapperUnRegisterEyeDataCallback(Marshal.GetFunctionPointerForDelegate((SRanipal_Eye.CallbackBasic)EyeCallback));
+            eye_callback_registered = false;
+        }
+    }
+
+    /// <summary>
+    /// Required class for IL2CPP scripting backend support
+    /// </summary>
+    internal class MonoPInvokeCallbackAttribute : System.Attribute
+    {
+        public MonoPInvokeCallbackAttribute() { }
+    }
+
+    /// <summary>
+    /// Eye tracking data callback thread.
+    /// Reports data at ~120hz
+    /// MonoPInvokeCallback attribute required for IL2CPP scripting backend
+    /// </summary>
+    /// <param name="eye_data">Reference to latest eye_data</param>
+    [MonoPInvokeCallback]
+    private static void EyeCallback(ref EyeData eye_data)
+    {
+        eyeData = eye_data;
+        // do stuff with eyeData...
+
+        lastTime = currentTime;
+        currentTime = eyeData.timestamp;
+    }
+}
